45878de2bab80e32db9609eb152d2050f568368a
Reynard lee <yylee.rey@gmail.com>
2021-12-17 15:30:06 +0800

    up to page 110

---
+++ src/components.rs
@@ -0,0 +1,10 @@
+pub use crate::prelude::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair, // helper class from bracket-lib that stores both a foreground and bg in a single struct
+    pub glyph: FontCharType, // store a single char / glyph
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Player; // empty struct, serves as a 'tag'
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod map;
 mod map_builder;
 mod camera;
+mod components;
+mod spawner;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
@@ -10,6 +12,8 @@ mod prelude {
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::spawner::*;
     pub use legion::*;
     pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
@@ -18,15 +22,23 @@ use prelude::*;
 struct State {
     // map: Map,
     // camera: Camera
+    ecs : World, // stores all entities and components
+    resources : Resources,
+    systems: Schedule // holds systems
 }
 impl State {
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start)
+            ecs,
+            resources,
+            systems: build_scheduler()
         }
     }
 }

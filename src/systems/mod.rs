e9d1a5f9840002fa57385bc8b4c9d5308659e3c0
Reynard lee <yylee.rey@gmail.com>
2022-01-25 08:56:05 +0800

    finish some tsutff

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -7,13 +7,52 @@ mod collisions;
 mod random_move;
 mod end_turn;
 
-pub fn build_scheduler() -> Schedule {
+/
+.flush()
+.add_system(collisions::collisions_system())
+.
+.flush()
+.add_system(collisions::collisions_system())
+.flush()
+.add_system()flush()
+.add_system()/ pub fn build_scheduler() -> Schedule {
+//     Schedule::builder()
+//         .add_system(player_input::player_input_system())
+//         .add_system(collisions::collisions_system())
+//         .flush() // tells Legion to apply changes immediately
+//         .add_system(map_render::map_render_system())
+//         .add_system(entity_render::entity_render_system())
+//         .add_system(random_move::random_move_system())
+//         .build()
+// }
+
+pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
-        .add_system(collisions::collisions_system())
         .flush() // tells Legion to apply changes immediately
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .build()
+}
+
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+pub fn build_monster_scheduler() -> Schedule {
+    Schedule::builder()
         .add_system(random_move::random_move_system())
+        .flush()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
